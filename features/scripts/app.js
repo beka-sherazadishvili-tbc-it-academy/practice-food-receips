ab00dad927ce7198e95b752f5b5a532a94463ffa
burdu <23202223@ibsu.edu.ge>
2025-11-09 20:52:59 +0400

    feat(dev): add hardcoded item

---
+++ features/scripts/app.js
@@ -0,0 +1,15 @@
+const myRecipes = [
+    {
+        title: 'Avocado & Egg Toast',
+        duration: '15m',
+        difficulty: 'Easy',
+        imageURL: 'https://www.dontgobaconmyheart.co.uk/wp-content/uploads/2022/05/avocado-egg-toast.jpg',
+        ingredients: {
+            avocado:10,
+            bread:2,
+            egg:1
+        },
+        weekDay: 'monday',
+        type: 'breakfast',
+    }
+];
