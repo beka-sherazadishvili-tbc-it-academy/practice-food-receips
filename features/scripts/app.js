9cf8f91de6714c20a031530340218312cfad1494
milena-mamadiani-tbc-it-academy <devwme0@gmail.com>
2025-11-09 22:39:06 +0400

    feat: add new recipe

--- features/scripts/app.js
+++ features/scripts/app.js
@@ -11,5 +11,18 @@ const myRecipes = [
         },
         weekDay: 'monday',
         type: 'breakfast',
+    },
+    {
+        title: 'Vegetable Salad',
+        duration: '15m',
+        difficulty: 'Easy',
+        imageURL: 'https://www.dontgobaconmyheart.co.uk/wp-content/uploads/2022/05/avocado-egg-toast.jpg',
+        ingredients: {
+            avocado:10,
+            bread:2,
+            egg:1
+        },
+        weekDay: 'monday',
+        type: 'breakfast',
     }
 ];
